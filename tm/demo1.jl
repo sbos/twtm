4ecc72d921cc83c76b717743c6113284de8837f6
sbos <sbos.net@gmail.com>
2013-03-24 11:26:54 +0400

    count_z and part of particle filter

--- tm/demo1.jl
+++ tm/demo1.jl
@@ -2,12 +2,22 @@ include("twtm.jl")
 using TwitterTopicModeling
 
 K = 3
-V = 15
+V = 20
 alpha = ones(K) * 0.1
 beta = 0.01
-T = 100
+T = 20
 p0 = 0.8
 
 phi = gen_phi(K, V, beta)
 theta = gen_theta(T, alpha, p0)
 docs = gen_docs(17, theta, phi)
+
+z = q_z(log(theta), log(phi), docs.colptr, docs.rowval)
+n = count_z(docs, z)
+
+est_theta = zeros(T, K)
+for t=1:T
+    est_theta[t, :] = n[t, :] / sum(n[t, :])
+end
+println("theta(z) reconstruction error (z estimated from true theta):")
+println(sum(abs(est_theta - theta), 1) / T)
--- tm/learning.jl
+++ tm/learning.jl
@@ -1,27 +1,43 @@
 require("Options")
 using OptionsMod
 
-function q_z(log_theta, log_phi, word_mask)
+function q_z(log_theta, log_phi, docptr, wordval)
     K, V = size(log_phi)
     T = size(log_theta, 1)
     z = Array(SparseMatrixCSC, T)
     for t = 1:T
-        N = size(word_mask[t], 1)
-        I = repmat([1:K], N)
-        J = vcat({ones(K) * v for v in word_mask[t]})
+        doc = wordval[docptr[t] : docptr[t+1] - 1]
+        N = length(doc)
+        I = vec(repmat([1:K], N, 1))
+        J = vcat([ones(Int, K) * w for w in doc]...)
         data = zeros(N * K)
         for v in 1:N
-            w = word_mask[v]
-            z_t = exp(log_theta[t, :] + log_phi[:, w])
-            z[v*K:(v+1)*K] = z_t ./ z_t.sum()
+            w = doc[v]
+            z_t = exp(log_theta[t, :]' + log_phi[:, w])
+            data[(v-1)*K + 1 : v*K] = z_t ./ sum(z_t)
         end
         z[t] = sparse(J, I, data, V, K)
     end
     return z
 end
 
+function count_z(docs, z)
+    V, T = size(docs)
+    V, K = size(z[1])
+    n = zeros(T, K)
+
+    for t=1:T
+        w = docs.nzval[docs.colptr[t]:docs.colptr[t+1]-1]
+        for k=1:K
+            z_k = z[t].nzval[z[t].colptr[k]:z[t].colptr[k+1]-1]
+            n[t, k] = dot(w, z_k)
+        end
+    end
+    return n
+end
+
 function q_theta(n, alpha, p0, L, opts::Options)
-    @defaults opts resample=true smooth=true
+    @defaults opts smoothing=true
     T, V = size(n)
     K = size(alpha, 1)
     
@@ -30,15 +46,79 @@ function q_theta(n, alpha, p0, L, opts::Options)
     pt = zeros(T, L)
     w = zeros(T, L)
 
-    let dir = Dirichlet(alpha + n[1, :]') 
-        theta[1, :] = rand(dir, L)
-        pt[1, :] = 0 
-# w[1, :] = vcat([logpdf(dir, theta[1, j]) for j=1:L]...)
+    function resample(w)
+        dist = Categorical(w)
+        return rand(dist, length(w))
     end
 
-    for i=2:T
-        let dir_t = Dirichlet(alpha + n[t, :]')
+    let q = Dirichlet(alpha + n[1, :]'),
+        pr = Dirichlet(alpha),
+        N = sum(n[1, :])
+        theta[1, :] = rand(q, L)
+        pt[1, :] = 0 
+        w[1, :] = logpdf(pr, theta[1, :, :]) + vcat([logpdf(Multinomial(N, theta[1, j, :]'), n[1, :]') - logpdf(q, theta[1, j, :]) for j=1:L]...)
     end
+
+    for t=2:T
+        idx = resample(exp(w[t-1, :]))
+        theta[t-1, :, :] = theta[t-1, idx, :]
+        w[t-1, :]        = w[t-1, idx]
+        pt[t-1, :]       = pt[t-1, idx]
+
+        let n = n[t, :]'
+            function pt(th)
+                a = p0 * prod(th .^ n)
+                b = (1-p0) * exp(log_dirmult(alpha) - log_dirmult(alpha + n))
+                return a / (a + b)
+            end
+
+            dir = Dirichlet(alpha + n)
+            function q(th)
+                pt = pt(th)
+                if rand(Binomial(1, pt), 1)[1] == 1
+                    return th, log(pt), pt
+                end
+                sample = vec(rand(dir, 1))
+                return sample, log(1-pt) + logpdf(dir, sample), pt
+            end
+
+            N = sum(n)
+            pdir = Dirichlet(alpha)
+            function g(th)
+                return sum(log(theta) .* n)
+            end
+
+            function f(currth, prevth)
+                if sum(abs(currth - prevth)) < 1e-10
+                    return log(p0)
+                end
+                return log(1-p0) + logpdf(pdir, currth)
+            end
+
+            for j=1:L
+                theta[t, j, :], w[t, j], pt[t, j] = propose(theta[t-1, j, :])
+                w[t, j] = w[t-1, j] + f(theta[t, j, :], theta[t-1, j, :]) + g(theta[t, j, :]) - w[t, j]
+            end
+        end
+    end
+
+    if smoothing == true
+        for t=N:-1:1
+            
+        end
+    end
+
+    log_theta    = zeros(T, K)
+    sample_theta = zeros(T, K)
+    sample_pt    = zeros(T)
+    for t=1:T
+        w = exp(w[t, :])
+        log_theta[t, :]    = sum(log(theta[t, :, :]) .* w)
+        sample_theta[t, :] = sum(    theta[t, :, :]  .* w)
+        sample_pt[t]       = sum(       pt[t, :]     .* w)
+    end
+
+    return log_theta, sample_theta, sample_pt
 end
 
 function M_step(n, beta)
@@ -47,4 +127,5 @@ function M_step(n, beta)
 
     return bsxfun(./, phi, sum(phi, 2))
 end
-    end
+
+export q_z, count_z, q_theta, M_step
